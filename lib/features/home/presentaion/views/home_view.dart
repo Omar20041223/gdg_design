c96cc5668908363a62c1d51274b1ddc64873637c
OmarMohamed <omarmohamed01284@gmail.com>
2024-11-18 15:26:52 +0200

    create the home view body

--- lib/features/home/presentaion/views/home_view.dart
+++ lib/features/home/presentaion/views/home_view.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:gdg_design/features/home/presentaion/views/widgets/home_view_body.dart';
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(child: HomeViewBody()),
+    );
   }
 }
